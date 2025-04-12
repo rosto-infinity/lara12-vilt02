2130568f52586d77a1bcf0decd03b9f39e91f909
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-12 03:10:53 +0100

    * 9-Show the confirm password page.

--- app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -13,7 +13,7 @@ use Inertia\Response;
 class ConfirmablePasswordController extends Controller
 {
     /**
-     * Show the confirm password page.
+     * 9-Show the confirm password page.
      */
     public function show(): Response
     {
