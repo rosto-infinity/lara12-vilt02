d352387bf469ac737b6efb4da2f04b259b68427c
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-12 03:11:51 +0100

    * 13-Show the email verification prompt page.

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -11,7 +11,7 @@ use Inertia\Response;
 class EmailVerificationPromptController extends Controller
 {
     /**
-     * Show the email verification prompt page.
+     * 13-Show the email verification prompt page.
      */
     public function __invoke(Request $request): RedirectResponse|Response
     {
