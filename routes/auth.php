be89376ce10db2766ae9749f1145f92ad597eba6
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-12 03:17:41 +0100

    29-Traite la soumission du formulaire de réinitialisation de mot de passe

--- routes/auth.php
+++ routes/auth.php
@@ -38,7 +38,7 @@ Route::middleware('guest')->group(function () {
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
         ->name('password.reset');
 
-    // Traite la soumission du formulaire de réinitialisation de mot de passe
+    // 29-Traite la soumission du formulaire de réinitialisation de mot de passe
     Route::post('reset-password', [NewPasswordController::class, 'store'])
         ->name('password.store');
 });
