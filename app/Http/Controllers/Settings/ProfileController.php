02f34c599dbd4be40dc2c2d3838874a54f5d060c
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-12 03:08:59 +0100

    * 3-Show the user's profile settings page.

--- app/Http/Controllers/Settings/ProfileController.php
+++ app/Http/Controllers/Settings/ProfileController.php
@@ -14,7 +14,7 @@ use Inertia\Response;
 class ProfileController extends Controller
 {
     /**
-     * Show the user's profile settings page.
+     * 3-Show the user's profile settings page.
      */
     public function edit(Request $request): Response
     {
